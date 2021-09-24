d6778dfc45be4c597a6188b8301bf7aa440ba1b2
Noah Warnock <noahwarnock5@gmail.com>
2021-09-24 14:34:42 -0400

    +Added characters +Added UI +Added Maps +Added GameModes and PlayerControllers and SaveGames

---
+++ Source/BTD/GameInfoInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInfoInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BTD_API UGameInfoInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
