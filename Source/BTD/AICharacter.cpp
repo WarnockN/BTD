dfef7b5bf763501c3bc9ca252a7275caaadbf5c7
Noah Warnock <noahwarnock5@gmail.com>
2021-10-06 15:58:40 -0400

    +Added AICharacter and AICharacterController class +Added AI functionality to see player within a 90 degree FOV *Changed GameplayDebugger to see AI status (Press Z to toggle GameplayDebugger on and off)

---
+++ Source/BTD/AICharacter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AICharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AAICharacter::AAICharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Have character face the direction it is walking, turns at 600 units per second along the Y axis.
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 600.0f, 0.0f);
+}
+
+// Called when the game starts or when spawned
+void AAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAICharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/BTD/AICharacterController.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AICharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BTD_API AAICharacterController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AAICharacterController();
+
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void Tick(float deltaSeconds) override;
+	virtual FRotator GetControlRotation() const override;
+
+	UFUNCTION()
+	void OnPawnDetected(const TArray<AActor*> &detectedPawns);
+
+	//How far the AI can see in a circle around it.
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+	float sightRadius = 500.0f;
+
+	//How long an object is kept in memory after it is detected
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+	float sightAge = 5.0f;
+
+	//AI loses right after being 50 units out of sightRadius
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+	float loseSightRadius = sightRadius + 50.0f;
+
+	//FOV of the AI in degrees
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+	float fov = 90.0f;
+
+	//Forward declaration of Unreal AI sight component
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+	class UAISenseConfig_Sight* sightConfig;
+	
+};
