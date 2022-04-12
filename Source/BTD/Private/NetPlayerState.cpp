b75a2df42964e39e2c848c2dd61f8bff586059ae
WarnockN <54872390+WarnockN@users.noreply.github.com>
2022-04-11 22:09:55 -0400

    Delete Source directory

--- Source/BTD/Private/NetPlayerState.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "NetPlayerState.h"
-
-void ANetPlayerState::AddScore(float ScoreDelta)
-{
-	SetScore(GetScore() + ScoreDelta);
-}
--- Source/BTD/ai_tags.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Runtime/Core/Public/UObject/NameTypes.h"
-#include "Runtime/Core/Public/Containers/UnrealString.h"
-
-namespace tags
-{
-	TCHAR const* const noise_tag = TEXT("Noise");
-}
